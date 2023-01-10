5ccf1f11f13472590838026952244038b9030d32
Christian Morris <116536697+DevChristianMorris@users.noreply.github.com>
2023-01-10 15:42:21 +1300

    Add files via upload

---
+++ 03-exercise---react-state-hook-ChristianMorrisDI-main/src/components/ReviewList.jsx
@@ -0,0 +1,17 @@
+import ReviewCard from './ReviewCard';
+import './ReviewList.css';
+
+const ReviewList = ({ counter, reviews }) => {
+
+const limitedArray = reviews.slice(0, counter);
+
+  return (
+    <div className="review-list">
+      {limitedArray.map((review, index) => {
+        return <ReviewCard key={index} review={review}/>
+      })}
+    </div>
+  );
+};
+
+export default ReviewList;
